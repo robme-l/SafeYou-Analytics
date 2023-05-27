3c52a023f14a357202649805b5e2757936fe673b
robme.l <robme.l@tuta.io>
2023-05-27 10:10:23 +0400

    Configured Static Root Directory

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'config' / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
